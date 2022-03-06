6bce8bd2ce8bf234ac7f35fef4701e6c39a4921e
Zokirjon Sotvoldiyev <91331822+developerZok@users.noreply.github.com>
2022-03-07 00:26:01 +0500

    new repo

---
+++ data.js
@@ -0,0 +1,116 @@
+const customers = [
+    {
+        name: "Neil Sims",
+        email: "email@windster.com",
+        price: "$320",
+        img: "../images/avatar1.png"
+    },
+    {
+        name: "Bonnie Green",
+        email: "email@example.com",
+        price: "$$67",
+        img: "../images/avatar2.png"
+    },
+    {
+        name: "Micheal Gough",
+        email: "email@example.com",
+        price: "$3467",
+        img: "../images/avatar3.png"
+    },
+    {
+        name: "Thomas Lean",
+        email: "email@example.com",
+        price: "$2367",
+        img: "../images/avatar4.png"
+    },
+    {
+        name: "Lana Byrd",
+        email: "email@example.com",
+        price: "$367",
+        img: "../images/avatar5.png"
+    },
+    {
+        name: "Karen Nelson",
+        email: "email@example.com",
+        price: "$1367",
+        img: "../images/avatar6.png"
+    },
+]
+
+const productsData = [
+    {
+        title: "Restaurant Booking App",
+        text: "React & Bootstrap Framework",
+        count: 70,
+    },
+    {
+        title: "UI Kit",
+        text: "React & Bootstrap Framework",
+        count: 54,
+    },
+    {
+        title: "Design System Pro",
+        text: "Bootstrap Framework",
+        count: 22,
+    },
+    {
+        title: "Dashboard",
+        text: "Tailwind, React",
+        count: 47,
+    },
+    {
+        title: "Glassmorphism UI",
+        text: "Vue Js, Tailwind ",
+        count: 43,
+    },
+    {
+        title: "Multipurpose Template",
+        text: "React & Bootstrap Framework",
+        count: 38,
+    },
+]
+
+const tables = [
+    {
+        transaction: "Payment from",
+        span: "Bonnie Green",
+        dateTime: "Apr 23 ,2021",
+        amount: "$2300",
+        status: "Completed"
+    },
+    {
+        transaction: "Payment refund to",
+        span: "#00910",
+        dateTime: "Apr 23 ,2021",
+        amount: "-$670",
+        status: "Completed"
+    },
+    {
+        transaction: "Payment failed from",
+        span: "#087651",
+        dateTime: "Apr 18 ,2021",
+        amount: "$234",
+        status: "Cancelled"
+    },
+    {
+        transaction: "Payment from",
+        span: "Bonnie Green",
+        dateTime: "Apr 15 ,2021",
+        amount: "$5000",
+        status: "In progress"
+    },
+    {
+        transaction: "Payment from",
+        span: "Jese Leos",
+        dateTime: "Apr 11 ,2021",
+        amount: "$2300",
+        status: "In progress"
+    },
+    {
+        transaction: "Payment from",
+        span: "THEMSBERG LLC",
+        dateTime: "Apr 11 ,2021",
+        amount: "$280",
+        status: "Completed"
+    },
+]
---
+++ src/main.js
@@ -0,0 +1,305 @@
+// button addEventListener
+
+const menuBtn = document.querySelector('#menu-btn')
+const dropdown = document.querySelector('#dropdown')
+const menuBtnTwo = document.querySelector('#menu-btnTwo')
+const dropdownTwo = document.querySelector('#dropdownTwo')
+const menuBtnThree = document.querySelector('#menu-btnThree')
+const dropdownThree = document.querySelector('#dropdownThree')
+const menuBtnMessage = document.querySelector('#menu-btnFour')
+
+const dropdownImg = document.querySelector('#bg__img--dropdown')
+const dropdownImgTwo = document.querySelector('#bg__img--dropdownTwo')
+const dropdownImgThree = document.querySelector('#bg__img--dropdownThree')
+const dropdownImgFour = document.querySelector('#bg__img--dropdownFour')
+
+let menuIsHiddenOne = false;
+let menuIsHiddenTwo = false;
+let menuIsHiddenThree = false;
+let menuIsHiddenFour = false;
+
+// one menu btn
+menuBtn.addEventListener('click', () => {
+    dropdown.classList.toggle('hidden')
+    dropdown.classList.toggle('flex')
+
+    if (menuIsHiddenOne == false) {
+        menuBtn.classList.add('bg-gray-100', 'rounded-xl')
+        dropdownImg.classList.add('bg__img--dropdownActive')
+        menuIsHiddenOne = true
+    } else {
+        menuBtn.classList.remove('bg-gray-100')
+        dropdownImg.classList.remove('bg__img--dropdownActive')
+        menuIsHiddenOne = false
+    }
+})
+// two menu btn
+menuBtnTwo.addEventListener('click', () => {
+    dropdownTwo.classList.toggle('hidden')
+    dropdownTwo.classList.toggle('flex')
+
+    if (menuIsHiddenTwo == false) {
+        menuBtnTwo.classList.add('bg-gray-100', 'rounded-xl')
+        dropdownImgTwo.classList.add('bg__img--dropdownActive')
+        menuIsHiddenTwo = true
+    } else {
+        menuBtnTwo.classList.remove('bg-gray-100')
+        dropdownImgTwo.classList.remove('bg__img--dropdownActive')
+        menuIsHiddenTwo = false
+    }
+})
+// three menu btn
+menuBtnThree.addEventListener('click', () => {
+    dropdownThree.classList.toggle('hidden')
+    dropdownThree.classList.toggle('flex')
+
+    if (menuIsHiddenThree == false) {
+        menuBtnThree.classList.add('bg-gray-100', 'rounded-xl')
+        dropdownImgThree.classList.add('bg__img--dropdownActive')
+        menuIsHiddenThree = true
+    } else {
+        menuBtnThree.classList.remove('bg-gray-100')
+        dropdownImgThree.classList.remove('bg__img--dropdownActive')
+        menuIsHiddenThree = false
+    }
+})
+//  four menu btn
+menuBtnMessage.addEventListener('click', () => {
+    if (menuIsHiddenFour === false) {
+        menuBtnMessage.classList.add('bg-gray-100', 'rounded-xl')
+        menuIsHiddenFour = true
+    } else {
+        menuBtnMessage.classList.remove('bg-gray-100')
+        menuIsHiddenFour = false
+    }
+
+})
+// chart js
+
+const ctx = document.querySelector('#myChart').getContext("2d");
+
+const labels = [
+    "01 Apr",
+    "02 Apr",
+    "03 Apr",
+    "04 Apr",
+    "05 Apr",
+    "06 Apr",
+    "07 Apr",
+    "08 Apr",
+    "09 Apr",
+    "10 Apr",
+];
+
+const data = {
+    labels,
+    datasets: [{
+        data: [30, 40, 50, 45, 60, 40, 50, 45, 50, 40],
+        label: "Sales",
+        fill: true,
+        borderColor: "green",
+        tension: 0.4,
+    }, ],
+};
+
+const config = {
+    type: "line",
+    data: data,
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                ticks: {
+                    callback: function (value) {
+                        return value + "K";
+                    },
+                },
+            },
+        },
+    },
+};
+
+const myChart = new Chart(ctx, config);
+// end off chart js
+
+// customers date
+const customerBox = document.querySelector("#customer__box");
+
+function Customerdata(data, customerBox) {
+    data.map(elem => {
+
+        // createlemts
+        let ul = document.createElement("ul");
+        let li = document.createElement("li");
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        let divNE = document.createElement("div");
+        let pName = document.createElement("p");
+        let pEmail = document.createElement("p");
+        let divPrice = document.createElement("div");
+
+        // add classlists
+        ul.classList.add("divide-y", "divide-gray-200", "dark:divide-gray-700")
+        li.classList.add("py-3", "sm:py-4")
+        div.classList.add("flex", "items-center", "space-x-4");
+        img.classList.add("w-8", "h-8", "rounded-full");
+        divNE.classList.add("flex-1", "min-w-0");
+        pName.classList.add("text-sm", "font-medium", "text-gray-900");
+        pEmail.classList.add("text-sm", "text-gray-500", "dark:text-gray-400");
+        divPrice.classList.add("inline-flex", "items-center", "text-base", "font-semibold", "text-gray-900")
+
+        // attachs
+        pName.textContent = elem.name;
+        pEmail.textContent = elem.email;
+        divPrice.textContent = elem.price;
+        img.setAttribute("src", elem.img)
+
+        // appends
+        customerBox.appendChild(ul);
+        ul.appendChild(li);
+        li.appendChild(div);
+        div.appendChild(img);
+        div.appendChild(divNE);
+        divNE.appendChild(pName);
+        divNE.appendChild(pEmail);
+        div.appendChild(divPrice)
+    })
+}
+Customerdata(customers, customerBox);
+
+// top product date
+
+const topProducts = document.querySelector("#top__product");
+
+function products(data, topProducts) {
+    data.map(elem => {
+        // createlements
+        let ul = document.createElement("ul");
+        let li = document.createElement("li");
+        let div = document.createElement("div");
+        let divNE = document.createElement("div");
+        let pName = document.createElement("p");
+        let pEmail = document.createElement("p");
+        let divPrice = document.createElement("div");
+        let sales = document.createElement("p");
+
+        // add classlists
+        ul.classList.add("divide-y", "divide-gray-200", "dark:divide-gray-700")
+        li.classList.add("py-3", "sm:py-4")
+        div.classList.add("flex", "items-center", "space-x-4");
+        divNE.classList.add("flex-1", "min-w-0");
+        pName.classList.add("text-sm", "font-medium", "text-gray-900");
+        pEmail.classList.add("text-sm", "text-gray-500", "dark:text-gray-400");
+        divPrice.classList.add("inline-flex", "items-center", "text-base", "font-semibold", "text-gray-900")
+        sales.classList.add("ml-2", "dark:text-gray-400")
+        // attachs
+        pName.textContent = elem.title;
+        pEmail.textContent = elem.text;
+        divPrice.textContent = elem.count;
+        sales.textContent = "sales";
+
+        // appends
+        topProducts.appendChild(ul);
+        ul.appendChild(li);
+        li.appendChild(div);
+        div.appendChild(divNE);
+        divNE.appendChild(pName);
+        divNE.appendChild(pEmail);
+        div.appendChild(divPrice)
+        divPrice.appendChild(sales)
+    })
+}
+products(productsData, topProducts);
+
+// tables 
+
+const tbody = document.querySelector("#tbody");
+
+function table(data, tbody) {
+    data.map(elem => {
+
+        // createlements
+        let tr = document.createElement("tr");
+        let tdTitle = document.createElement("td");
+        let tdData = document.createElement("td");
+        let tdPrise = document.createElement("td");
+        let tdStatus = document.createElement("td");
+        let pTitle = document.createElement("p");
+        let pData = document.createElement("p");
+        let pPrise = document.createElement("p");
+        let spanStatus = document.createElement("span");
+        let spanBox = document.createElement("span");
+        let spanArea = document.createElement("span");
+        let pTitleSpan = document.createElement("span");
+
+        // add classlists
+        tdTitle.classList.add("px-4", "sm:px-5", "py-5", "text-xs", "sm:text-sm");
+        tdData.classList.add("text-xs", "sm:text-sm");
+        tdPrise.classList.add("md:px-5", "text-xs", "py-5", );
+        tdStatus.classList.add("px-5", "text-xs", "py-5", "hidden", "md:inline-block");
+
+        pTitle.classList.add("text-gray-900");
+        pTitleSpan.classList.add("font-semibold", "ml-1");
+
+        pPrise.classList.add("text-gray-900", "font-semibold", "hidden", "md:inline-block");
+
+        spanBox.classList.add("relative", "px-3", "py-1", "font-semibold", "text-green-900");
+        spanArea.classList.add("absolute", "inset-0", "bg-green-200", "opacity-50", "rounded-full");
+        spanStatus.classList.add("relative");
+
+        // attachs
+        pTitle.textContent = elem.transaction;
+        pTitleSpan.textContent = elem.span;
+        pData.textContent = elem.dateTime;
+        pPrise.textContent = elem.amount;
+        spanStatus.textContent = elem.status;
+
+        // appends
+        tbody.appendChild(tr);
+        tr.appendChild(tdTitle);
+        tdTitle.appendChild(pTitle);
+        pTitle.appendChild(pTitleSpan);
+
+        tr.appendChild(tdData);
+        tdData.appendChild(pData);
+
+        tr.appendChild(tdPrise);
+        tdPrise.appendChild(pPrise);
+
+        tr.appendChild(tdStatus);
+        tdStatus.appendChild(spanBox);
+        spanBox.appendChild(spanArea);
+        spanBox.appendChild(spanStatus);
+
+        // styles
+        if (elem.status === "Cancelled") {
+            spanBox.classList.add("relative", "px-3", "py-1", "font-semibold", "text-red-600");
+            spanArea.classList.add("absolute", "inset-0", "bg-pink-300", "opacity-50", "rounded-full");
+        }
+        if (elem.status === "In progress") {
+            spanBox.classList.add("relative", "px-3", "py-1", "font-semibold", "text-blue-600");
+            spanArea.classList.add("absolute", "inset-0", "bg-blue-200", "opacity-50", "rounded-full");
+        }
+
+        tr.classList.add("tbody__tr")
+    })
+}
+table(tables, tbody);
+
+// hamburger menu 
+
+const navbarBtn = document.querySelector(".navbar__btn");
+const navbarMenu = document.querySelector("#navbar__menu");
+const navbarBtnClose = document.querySelector(".navbar__btn--close")
+
+navbarBtn.addEventListener("click", () => {
+
+    navbarMenu.classList.add("show")
+    navbarMenu.classList.remove("hide")
+    navbarBtnClose.classList.add("navbar__btn--show")
+})
+navbarBtnClose.addEventListener("click", () => {
+    navbarMenu.classList.remove("show")
+    navbarMenu.classList.add("hide")
+})
+// end off hamburger menu
